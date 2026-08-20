c62abb8b40389a689ddef3c904b9bc1f59eb366e
Tercio Gaudencio Filho <terciofilho@gmail.com>
2026-08-20 09:23:58 -0300

    [FIX] Ghosthing suggestions are overwriting text in front of the cursor. Also make it same font so when completing there is no text shift.

--- GaussTests/AppTests.swift
+++ GaussTests/AppTests.swift
@@ -1,7 +1,37 @@
 import XCTest
+@testable import Gauss
 
 final class AppTests: XCTestCase {
-    func testPlaceholder() {
-        XCTAssertTrue(true)
+
+    func testGhostShownWhenTextFollowsCursorOnSameLine() {
+        let text = "1 Braz in US dollar" as NSString
+        let cursor = ("1 Braz" as NSString).length
+        XCTAssertTrue(CalcTextView.shouldShowGhost(in: text, cursorPos: cursor))
+    }
+
+    func testGhostShownAtEndOfLine() {
+        let text = "1 Braz" as NSString
+        XCTAssertTrue(CalcTextView.shouldShowGhost(in: text, cursorPos: text.length))
+    }
+
+    func testGhostShownBeforeNewline() {
+        let text = "1 Braz\n2 + 2" as NSString
+        let cursor = ("1 Braz" as NSString).length
+        XCTAssertTrue(CalcTextView.shouldShowGhost(in: text, cursorPos: cursor))
+    }
+
+    func testGhostShownWhenOnlyTrailingWhitespace() {
+        let text = "1 Braz  " as NSString
+        let cursor = ("1 Braz" as NSString).length
+        XCTAssertTrue(CalcTextView.shouldShowGhost(in: text, cursorPos: cursor))
+    }
+
+    func testGhostHiddenInMiddleOfWord() {
+        let text = "Brazilian" as NSString
+        XCTAssertFalse(CalcTextView.shouldShowGhost(in: text, cursorPos: 4))
+    }
+
+    func testGhostHiddenAtStart() {
+        XCTAssertFalse(CalcTextView.shouldShowGhost(in: "Braz", cursorPos: 0))
     }
 }
